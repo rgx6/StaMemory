9fae46a973562f6f3bbcb4098ed01588d776dcbd
rgx6 <rgx@hotmail.co.jp>
2026-08-25 23:01:10 +0900

    dotnet8からdotnet10に移行

--- deploy-lambda.bat
+++ deploy-lambda.bat
@@ -1,5 +1,5 @@
 del publish.zip
-del /q .\api\bin\Release\net8.0\linux-arm64\publish\* 
+del /q .\api\bin\Release\net10.0\linux-arm64\publish\* 
 dotnet publish -c Release -r linux-arm64 --no-self-contained .\api
-7za a publish.zip .\api\bin\Release\net8.0\linux-arm64\publish\*
+7za a publish.zip .\api\bin\Release\net10.0\linux-arm64\publish\*
 aws lambda update-function-code --function-name StaMemory --zip-file fileb://publish.zip
